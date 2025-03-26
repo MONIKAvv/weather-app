d8408df1970a73568c6fc3ed11abcdc6e26203a0
MONIKAvv <monikalonewolf35@gmail.com>
2025-03-26 08:44:24 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,72 @@
-import { useState } from 'react'
-
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import search from "./assets/icons/search.svg";
+import { useStateContext } from "./context";
+import MiniCards from "./components/MiniCards";
+import Background from "./components/Background";
+import WeatherCard from "./components/WeatherCard";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [input, setInput] = useState("");
+  // context state management
+  const { weather, thisLocation, values, place, setPlace } = useStateContext();
+
+  const submitCity = () => {
+    setPlace(input);
+    setInput("");
+  };
 
   return (
-    <div>
-      <h1>This Monika Here</h1>
+    <div className="w-full h-screen text-white px-8">
+      <nav className="w-full p-5 flex justify-between items-center">
+        <h1 className="font-bold tracking-wide text-3xl">
+          Weather Application
+        </h1>
+        <div className="bg-white w-[15rem] overflow-hidden shadow-2xl rounded flex items-center p-2 gap-2">
+          <img src={search} alt="search" className="w-[1.5rem] h-[1.5rem]" />
+          <input
+            onKeyUp={(e) => {
+              if (e.key === "Enter") {
+                //submit the form
+                submitCity();
+              }
+            }}
+            type="text"
+            placeholder="Search City"
+            className="focus:outline-none w-full text-[#212121] text-lg"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+        </div>
+      </nav>
+      <Background />
+      <div className="flex"></div>
+      <main className="w-full flex justify-center items-center gap-8 py-20 px-[10%]">
+        <WeatherCard
+          place={thisLocation}
+          windspeed={weather.wspd}
+          humidity={weather.humidity}
+          temperature={weather.temp}
+          heatIndex={weather.heatIndex}
+          iconString={weather.conditions}
+          conditions={weather.conditions}
+        />
+        <div className="flex justify-center gap-8 flex-wrap w-[60%]">
+          {/* all forcasted value in values */}
+          {values?.slice(1, 7).map((current) => {
+            return (
+              <MiniCards
+                key={current.datetime}
+                time={current.datetime}
+                temp={current.temp}
+                iconString={current.conditions}
+              />
+            );
+          })}
+        </div>
+      </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -1,7 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Clear from '../assets/images/clear.png'
+import Fog from '../assets/images/fog2.png'
+import Cloudy from '../assets/images/cloudy.png'
+import Rainy from '../assets/images/rain.png'
+import Snow from '../assets/images/snow.png'
+import Stormy from '../assets/images/storm.png'
+import { useStateContext } from '../context'
 
 export default function Background() {
+  const {weather} = useStateContext();
+  const [image, setImage] = useState('Clear');
+
+  useEffect(() => {
+    if(weather.conditions){
+      let imageAsCond = weather.conditions
+      if(imageAsCond.toLowerCase().includes('clear')){
+        setImage(Clear)
+      }else if(imageAsCond.toLowerCase().includes('cloud')){
+        setImage(Cloudy)
+      }else if(imageAsCond.toLowerCase().includes('rain')){
+        setImage(Rainy)
+      }else if(imageAsCond.toLowerCase().includes('snow')){  //includes checks that the string is present or not and toLowercase case ko insensitive bana deta h 
+        setImage(Snow)
+      }else if(imageAsCond.toLowerCase().includes('fog')){
+        setImage(Fog)
+      }else if(imageAsCond.toLowerCase().includes('thunder') || imageAsCond.toLowerCase().includes('storm'))
+        setImage(Stormy)
+    }
+
+  }, [weather])
+
   return (
-    <div>Background</div>
+    <img
+    src={image}
+    alt='image'
+    className='fixed left-0 top-0 -z-[10]'
+    />
   )
 }
